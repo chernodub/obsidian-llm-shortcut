6d0c41b91f140adf666976f5cd87b88cd137d38c
senaev <senaev@yandex-team.ru>
2025-11-23 09:39:09 +0000

    default message

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,7 @@ import {
   TFolder,
 } from "obsidian";
 import { mapLlmErrorToReadable } from "./llm/error-handler";
-import { LLMClient, LLMClientSelectionParams } from "./llm/llm-client";
+import { LLMClient, SelectionParams } from "./llm/llm-client";
 import { logger } from "./logger";
 import { SettingTab } from "./setting-tab";
 import { LoaderStrategy, LoaderStrategyFactory } from "./ui/loader-strategy";
@@ -251,7 +251,7 @@ export default class LlmShortcutPlugin extends Plugin {
   }: {
     readonly userPromptString: string;
     readonly editor: Editor;
-    readonly selection: LLMClientSelectionParams;
+    readonly selection: SelectionParams;
   }) {
     assertExists(this.llmClient, "LLM client is not initialized");
 
