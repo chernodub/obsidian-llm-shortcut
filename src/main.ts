5dc3f09fa70cc12179e68fda260a223ca5394c0d
senaev <senaev@yandex-team.ru>
2025-11-26 11:50:02 +0000

    default message

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,6 @@ import {
   Editor,
   EditorPosition,
   EventRef,
-  FrontMatterCache,
   Plugin,
   PluginManifest,
   TAbstractFile,
@@ -19,86 +18,12 @@ import { LoaderStrategy, LoaderStrategyFactory } from "./ui/loader-strategy";
 import { CustomPromptModal } from "./ui/prompt-modal/prompt-modal";
 import { showErrorNotification } from "./ui/user-notifications";
 import { assertExists } from "./utils/assertions/assert-exists";
-import { PLUGIN_NAME } from "./utils/constants";
+import { DEFAULT_USER_PROMPT_OPTIONS, PLUGIN_NAME } from "./utils/constants";
 import { mapCursorPositionToIdx } from "./utils/obsidian/map-position-to-idx";
 import { obsidianFetchAdapter } from "./utils/obsidian/obsidian-fetch-adapter";
+import { parseUserPromptOptionsFromFileProperties } from "./utils/parseUserPromptOptionsFromFileProperties/parseUserPromptOptionsFromFileProperties";
 
-const SELECTION_MODE_PROP_NAME = "llm-shortcut-selection-mode";
-const SELECTION_ONLY_PROP_VALUE = "selection-only";
-
-const CONTEXT_SIZE_BEFORE_SELECTION_PROP_NAME =
-  "llm-shortcut-context-size-before-selection";
-const CONTEXT_SIZE_AFTER_SELECTION_PROP_NAME =
-  "llm-shortcut-context-size-after-selection";
-
-function parseNumericFileProperty(
-  fileProperties: FrontMatterCache,
-  propertyName: string,
-): number | undefined {
-  const value = fileProperties[propertyName];
-
-  if (value === undefined) {
-    return undefined;
-  }
-
-  if (typeof value !== "string") {
-    throw new Error(
-      `Invalid prompt file property=[${propertyName}] unknown value type [${value}]`,
-    );
-  }
-
-  const int = parseInt(value, 10);
-  if (int.toString(10) !== value) {
-    throw new Error(
-      `Invalid prompt file property=[${propertyName}] value should be an integer, but got [${value}]`,
-    );
-  }
-
-  if (int < 0) {
-    throw new Error(
-      `Invalid prompt file property=[${propertyName}] value should be positive, but got [${value}]`,
-    );
-  }
-
-  return int;
-}
-
-function parseUserPromptOptionsFromFileProperties(
-  fileProperties: FrontMatterCache,
-): UserPromptOptions {
-  let shouldHandleSelectionOnly: boolean;
-  const shouldHandleSelectionOnlyValue =
-    fileProperties[SELECTION_MODE_PROP_NAME];
-  if (shouldHandleSelectionOnlyValue === undefined) {
-    shouldHandleSelectionOnly = false;
-  } else if (shouldHandleSelectionOnlyValue === SELECTION_ONLY_PROP_VALUE) {
-    shouldHandleSelectionOnly = true;
-  } else {
-    throw new Error(
-      `Invalid prompt file property=[${SELECTION_MODE_PROP_NAME}] value should be [${SELECTION_ONLY_PROP_VALUE}], but got [${shouldHandleSelectionOnlyValue}]`,
-    );
-  }
-
-  return {
-    shouldHandleSelectionOnly,
-    contextSizeBeforeSelection: parseNumericFileProperty(
-      fileProperties,
-      CONTEXT_SIZE_BEFORE_SELECTION_PROP_NAME,
-    ),
-    contextSizeAfterSelection: parseNumericFileProperty(
-      fileProperties,
-      CONTEXT_SIZE_AFTER_SELECTION_PROP_NAME,
-    ),
-  };
-}
-
-const DEFAULT_USER_PROMPT_OPTIONS: UserPromptOptions = {
-  shouldHandleSelectionOnly: false,
-  contextSizeBeforeSelection: undefined,
-  contextSizeAfterSelection: undefined,
-};
-
-interface UserPromptOptions {
+export interface UserPromptOptions {
   readonly shouldHandleSelectionOnly: boolean;
   readonly contextSizeBeforeSelection: number | undefined;
   readonly contextSizeAfterSelection: number | undefined;
--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,2 +1,10 @@
+import { UserPromptOptions } from "../main";
+
 export const NEWLINE_SYMBOL = "\n";
 export const PLUGIN_NAME = "LLM Shortcut";
+
+export const DEFAULT_USER_PROMPT_OPTIONS: UserPromptOptions = {
+  shouldHandleSelectionOnly: false,
+  contextSizeBeforeSelection: undefined,
+  contextSizeAfterSelection: undefined,
+};
